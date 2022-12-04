22e3fce185f06734434be883152ff8ae3e51dd83
tunayaman <119753058+tunayaman@users.noreply.github.com>
2022-12-04 18:49:09 +0300

    Update execise_complete.c

--- chapter_9.3/execise_complete.c
+++ chapter_9.3/execise_complete.c
@@ -1,13 +1,24 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <math.h>
 
 
-class Solution{
-//     your code here
-        public:
-        double myPow(double x, int n) {
+void powerNumbers(float baseNumber, int exponent)
+{
+    float result;
+    result = pow(baseNumber,exponent);
 
-        }
-};
+    printf("result = %.3f",result);
+}
 
-//     your code here
+int main()
+{
+    int baseNumber,exponent;
+
+    printf("enter the base number: ");
+    scanf("%d",&baseNumber);
+    printf("enter the exponent: ");
+    scanf("%d",&exponent);
+
+    powerNumbers(baseNumber,exponent);
+
+}
